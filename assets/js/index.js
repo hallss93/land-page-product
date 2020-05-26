2c007c0a520ad7dd4b4c62c63621d59f749fdf13
Hallison Melo <hallss93@hotmail.com>
2020-05-26 16:25:14 -0300

    Image Notebook Blue

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,12 @@ setTimeout(() => {
     document.querySelector('.container-yellow').classList.add('expand')
     document.querySelector('.container .half img.notebook-yellow').classList.add('show')
     document.querySelector('.info').classList.add('show')
+    setTimeout(() => {
+        const offsetWidth = document.querySelector('.container-yellow.expand').offsetWidth;
+        console.log(offsetWidth);
+        const imgBlue = document.querySelector('div.container-yellow.expand > img.notebook-blue')
+        imgBlue.style.right = "calc(" + offsetWidth + "px - 110px)"
+        imgBlue.classList.add('show');
+
+    }, 500);
 }, 1000);
