0700da12a7e0eef66e3b962967aeab6633437f55
Hallison Melo <hallss93@hotmail.com>
2020-05-26 15:54:51 -0300

    Start Page

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+    document.querySelector('.container-yellow').classList.add('expand')
+    document.querySelector('.container .half img.notebook-yellow').classList.add('show')
+    document.querySelector('.info').classList.add('show')
+}, 1000);
