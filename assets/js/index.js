7b5475c7a01bf6bed56d6568c7d672b01f938876
Hallison Melo <hallss93@hotmail.com>
2020-05-26 16:51:45 -0300

    Background linear

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,6 +10,21 @@ setTimeout(() => {
     setTimeout(() => {
         const offsetWidth = document.querySelector('.container-yellow.expand').offsetWidth;
         const imgBlue = document.querySelector('div.container-yellow.expand > img.notebook-blue')
+        const body = document.body
+        let linear = 85.0;
+        setTimeout(() => {
+
+            const time = setInterval(() => {
+                if (linear >= 82) {
+                    linear = linear - 0.1;
+
+                    body.style.background = "linear-gradient(90deg, #8ec5fc " + linear + "%, #fee140 85%)"
+                    console.log(linear)
+                } else {
+                    clearInterval(time);
+                }
+            }, 10);
+        }, 300);
 
         imgBlue.style.right = "calc(" + offsetWidth + "px - 110px)"
         imgBlue.classList.add('show');
